85ace089ea9015852e775e19a19641e4fc1194db
Anggit Septiansyah <anggitseptiansyah@gmail.com>
2026-03-30 13:55:30 +0700

    Membuat fungsi relationship pada semua model cash

--- app/Models/CashExpenseType.php
+++ app/Models/CashExpenseType.php
@@ -4,9 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+use function PHPSTORM_META\map;
 
 class CashExpenseType extends Model
 {
     /** @use HasFactory<\Database\Factories\CashExpenseTypeFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'name',
+        'description'
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function cashExpense(): HasMany
+    {
+        return $this->hasMany(CashExpense::class);
+    }
 }
--- app/Models/CashIncome.php
+++ app/Models/CashIncome.php
@@ -4,9 +4,33 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class CashIncome extends Model
 {
     /** @use HasFactory<\Database\Factories\CashIncomeFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'user_id', 
+        'cash_income_type_id',
+        'amount',
+        'note',
+        'date',
+    ];
+
+    protected $casts = [
+        'date' => 'date',
+        'amount' => 'decimal:2',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function cashIncomeType(): BelongsTo
+    {
+        return $this->belongsTo(cashIncomeType::class);
+    }
 }
