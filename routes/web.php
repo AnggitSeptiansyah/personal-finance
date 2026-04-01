8488177dc2c0d3cfd3112948c372f1e8f74f3933
Anggit Septiansyah <anggitseptiansyah@gmail.com>
2026-04-01 20:07:03 +0700

    Membuat route untuk view/pagecontroller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -12,6 +13,18 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    // cash
+    Route::get('/cash/income-types', [PageController::class, 'cashIncomeTypes'])->name('cash.income-types');
+    Route::get('/cash/incomes', [PageController::class, 'cashIncomes'])->name('cash.incomes');
+    Route::get('/cash/expense-types', [PageController::class, 'cashExpenseTypes'])->name('cash.expense-types');
+    Route::get('/cash/expenses', [PageController::class, 'cashExpenses'])->name('cash.expenses');
+
+    // bank
+    Route::get('/bank/incomes-types', [PageController::class, 'bankIncomeTypes'])->name('bank.income-types');
+    Route::get('/bank/incomes', [PageController::class, 'bankIncomes'])->name('bank.incomes');
+    Route::get('/bank/expense-types', [PageController::class, 'bankExpenseTypes'])->name('bank.expense-types');
+    Route::get('/bank/expenses', [PageController::class, 'bankExpenses'])->name('bank.expenses');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
